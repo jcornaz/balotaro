3e270bf41751c92043e70ce31d219cf0e30a5671
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2016-09-25 17:08:12 +0200

    Add a test

--- src/test/kotlin/balotenko/BalotaroApplicationTests.kt
+++ src/test/kotlin/balotenko/BalotaroApplicationTests.kt
@@ -1,16 +1,36 @@
 package balotenko
 
+import balotenketo.balotaro.Balotaro
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
+@SpringBootTest(classes = arrayOf(Balotaro::class))
+@WebAppConfiguration
 class BalotaroApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Autowired
+    lateinit var webApplicationContext: WebApplicationContext
 
+    lateinit var mockMvc: MockMvc
+
+    @Before
+    fun setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build()
+    }
+
+    @Test
+    fun testGetRoot() {
+        mockMvc.perform(get("/")).andExpect(status().isOk)
+    }
 }
