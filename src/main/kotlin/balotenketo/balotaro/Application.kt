8d1b837095252ae478f60a8069ee8725ff7a582d
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2016-09-27 21:09:46 +0200

    Add swagger

--- src/main/kotlin/balotenketo/balotaro/Application.kt
+++ src/main/kotlin/balotenketo/balotaro/Application.kt
@@ -2,9 +2,48 @@ package balotenketo.balotaro
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
-open class Balotaro
+@EnableSwagger2
+open class Balotaro {
+
+    companion object {
+        val title = "Balotaro"
+        val description = "Vote web service using the condorcet method"
+        val authors = listOf("Jonathan Cornaz")
+        val license = "GNU General Public License Version 3"
+        val licenseUrl = "https://raw.githubusercontent.com/slimaku/balotaro/master/LICENSE"
+        val version = Version
+    }
+
+    @Bean
+    open fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2).apply {
+            groupName(groupName)
+            
+            apiInfo(ApiInfoBuilder().apply {
+                title(title)
+                description(description)
+                contact(authors.joinToString(", "))
+                license(license)
+                licenseUrl(licenseUrl)
+                version(version.toString())
+            }.build())
+
+            select().apply {
+                apis(RequestHandlerSelectors.any())
+                paths(PathSelectors.any())
+            }.build()
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(Balotaro::class.java, *args)
--- src/main/kotlin/balotenketo/balotaro/controller/ServiceController.kt
+++ src/main/kotlin/balotenketo/balotaro/controller/ServiceController.kt
@@ -1,5 +1,6 @@
 package balotenketo.balotaro.controller
 
+import balotenketo.balotaro.Balotaro
 import balotenketo.balotaro.Version
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,8 +11,8 @@ class ServiceController {
 
     @RequestMapping("/")
     fun getService() = object {
-        val service = "Balotaro"
-        val description = "Vote web service using the condorcet method"
+        val service = Balotaro.title
+        val description = Balotaro.description
         val version = Version.toString()
     }
 
